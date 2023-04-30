be3e135c4fe71b6c248bb3fdb38dfbee6a6e8d11
Samarth Rao <raosamarth1234@gmail.com>
2023-04-30 17:26:15 +0530

    Add components folder with About Home and Navbar Component. Set up React-Router-Dom. Create Navbar on Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { About } from './components/About';
+import { Home } from './components/Home';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <h1>This is iNotebook</h1>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element = {<Home />} />
+        <Route exact path='/about' element = {<About />}/>
+      </Routes>
+    </Router>
     </>    
   );
 }
